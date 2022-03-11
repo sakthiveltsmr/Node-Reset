562a204620ea9d5b58a8bd33efc146fb8b13907d
sakthiveltsmr <sakthiveltsmr@gmail.com>
2022-03-11 16:56:39 +0530

    schema

--- share/schema.js
+++ share/schema.js
@@ -4,12 +4,11 @@ const schema = {
   registerSchema: joi.object({
     name: joi.string().required(),
     email: joi.string().email().required(),
-    password: joi.string().alphanum().min(3).max(15).required(),
+    password: joi.string().alphanum().min(6).max(12).required(),
   }),
   loginSchema: joi.object({
     email: joi.string().email().required(),
-    password: joi.string().alphanum().min(3).max(12).required(),
+    password: joi.string().alphanum().min(6).max(12).required(),
   }),
 };
-
 module.exports = schema;
