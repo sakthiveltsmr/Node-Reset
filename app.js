1d1a0ccabcf31391f8b1b4f11470ec4ae7d75ced
sakthiveltsmr <sakthiveltsmr@gmail.com>
2022-03-11 17:44:39 +0530

    app

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ const express = require("express");
 const mongo = require("./share/mongodb");
 
 const userRoute = require("./Routes/users.Routes");
-// const resetRoute = require("./Routes/resetPass.routes");
+const resetRoute = require("./Routes/reset.Routes");
 
 const app = express();
 
@@ -17,15 +17,15 @@ const app = express();
     app.use(cors());
     app.use(express.json());
 
-    // /    app.use((req, res, next) => {
-    //       console.log("user middle ware called");
-    //       // res.send("server running");
-    //       next();
-    //     });
+    app.use((req, res, next) => {
+      console.log("user middle ware called");
+
+      next();
+    });
 
     app.use("/users", userRoute);
 
-    // app.use("/resetpassword", resetRoute);
+    app.use("/resetpassword", resetRoute);
 
     const port = process.env.PORT;
     app.listen(port, () => console.log(`server running at ${port}`));
