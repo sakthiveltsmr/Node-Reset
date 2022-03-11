69d07d0e583c417892ed53095477850eba5ba67f
sakthiveltsmr <sakthiveltsmr@gmail.com>
2022-03-11 16:54:04 +0530

    added

--- app.js
+++ app.js
@@ -1,28 +1,35 @@
 require("dotenv").config();
-const express = require("express");
-
-const db = require("./share/mongodb");
 
 const cors = require("cors");
-const userRoutes = require("./Routes/user.Routes");
+
+const express = require("express");
+
+const mongo = require("./share/mongodb");
+
+const userRoute = require("./Routes/user.Routes");
+// const resetRoute = require("./Routes/resetPass.routes");
+
 const app = express();
 
 (async () => {
   try {
-    await db.connect();
+    await mongo.connect();
     app.use(cors());
     app.use(express.json());
+
     app.use((req, res, next) => {
-      console.log("user middle ware");
+      console.log("user middle ware called");
+      // res.send("server running");
       next();
     });
-    app.use("/user", userRoutes);
 
-    const PORT = process.env.PORT;
-    app.listen(PORT, () => {
-      console.log(`sevrver running at ${PORT}`);
-    });
+    app.use("/user", userRoute);
+
+    // app.use("/resetpassword", resetRoute);
+
+    const port = process.env.PORT;
+    app.listen(port, () => console.log(`server running at ${port}`));
   } catch (err) {
-    console.log("error", err);
+    console.log("connection failiure", err.message);
   }
 })();
